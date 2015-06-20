dd67bc1a75f942c19f9984a10ef5285b8eb382ae
mikakane <mikakane2@gmail.com>
2015-06-20 10:33:13 +0900

    update

--- contents/assets/js/setting.js
+++ contents/assets/js/setting.js
@@ -6,26 +6,29 @@ document.getElementsByTagName( 'head' )[0].appendChild( link );
 
 
 document.fonts.addEventListener('loadingdone', function() {
-    console.log("hoge")
+    console.log("fonts-loaded");
+    // Full list of configuration options available at:
+    // https://github.com/hakimel/reveal.js#configuration
+    Reveal.initialize({
+        controls: true,
+        progress: true,
+        history: true,
+        center: true,
+
+        transition: 'fade', // none/fade/slide/convex/concave/zoom
+
+        // Optional reveal.js plugins
+        dependencies: [
+            { src: '/bower_components/reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+            { src: '/bower_components/reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+            { src: '/bower_components/reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+            { src: '/bower_components/reveal.js/plugin/highlight/highlight.js', condition: function() { return true;/*!!document.querySelector( 'pre code' );*/ }, callback: function() { hljs.initHighlightingOnLoad(); } },
+            { src: '/bower_components/reveal.js/plugin/zoom-js/zoom.js', async: true },
+            { src: '/bower_components/reveal.js/plugin/notes/notes.js', async: true }
+        ]
+    });
+    Reveal.addEventListener( 'ready', function( event ) {
+        $(".spinner").fadeOut();
+    } );
 });
 
-// Full list of configuration options available at:
-// https://github.com/hakimel/reveal.js#configuration
-Reveal.initialize({
-    controls: true,
-    progress: true,
-    history: true,
-    center: true,
-
-    transition: 'fade', // none/fade/slide/convex/concave/zoom
-
-    // Optional reveal.js plugins
-    dependencies: [
-        { src: '/bower_components/reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
-        { src: '/bower_components/reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-        { src: '/bower_components/reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-        { src: '/bower_components/reveal.js/plugin/highlight/highlight.js', condition: function() { return true;/*!!document.querySelector( 'pre code' );*/ }, callback: function() { hljs.initHighlightingOnLoad(); } },
-        { src: '/bower_components/reveal.js/plugin/zoom-js/zoom.js', async: true },
-        { src: '/bower_components/reveal.js/plugin/notes/notes.js', async: true }
-    ]
-});
